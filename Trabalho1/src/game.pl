d3daa6a75e0b3b152ee6b2896aff208f7ca575d8
Spininador <nunojorge90@hotmail.com>
2016-10-24 11:47:18 +0100

    relato intercalar adicionado

---
+++ Trabalho1/src/game.pl
@@ -0,0 +1,89 @@
+tabuleiro([
+[['empty',[]],['green',['w','s','e']],['green',['sw','se','n']],['green',['sw','s','se']],['green',['sw','se','ne','nw']],['green',['sw','se','s']],['green',['sw','se','n']],['green',['w','s','e']],['empty',[]]],
+[['empty',[]],['empty',[]],['green',['s','se']],['green',['sw','se']],['green',['s','n']],['green',['sw','se']],['green',['sw','s']],['empty',[]],['empty',[]]],
+[['empty',[]],['empty',[]],['empty',[]],['green',['s']],['green',['s']],['green',['s']],['empty',[]],['empty',[]],['empty',[]]],
+[['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]]],
+[['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]]],
+[['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]],['empty',[]]],
+[['empty',[]],['empty',[]],['empty',[]],['red',['n']],['red',['n']],['red',['n']],['empty',[]],['empty',[]],['empty',[]]],
+[['empty',[]],['empty',[]],['red',['n','ne']],['red',['nw','ne']],['red',['s','n']],['red',['nw','ne']],['red',['nw','n']],['empty',[]],['empty',[]]],
+[['empty',[]],['red',['w','n','e']],['red',['nw','ne','s']],['red',['nw','n','ne']],['red',['sw','se','ne','nw']],['red',['nw','ne','n']],['red',['nw','ne','s']],['red',['w','n','e']],['empty',[]]]]).
+
+ 
+draw_gameboard:-X^(tabuleiro(X),draw_board(X)).
+ 
+draw_board(Tab):-
+	draw_straightLine,nl,
+	draw_lines(Tab).
+	
+draw_lines([]).
+draw_lines([LIN|REST]):-(
+	write('|'),draw_line1(LIN),nl,
+	write('|'),draw_line2(LIN),nl,
+	write('|'),draw_line3(LIN),nl,
+	draw_straightLine,
+	nl,
+	draw_lines(REST)).
+
+draw_line1([]).
+draw_line1([SQUARE|REST]):-
+	draw_line1sq1(SQUARE),
+	draw_line1sq2(SQUARE),
+	draw_line1sq3(SQUARE),
+	write('|'),
+	draw_line1(REST).
+ 
+draw_line1sq1([]).
+draw_line1sq1([TEAM|[SIDES|NONE]]):-
+	member('nw',SIDES) -> write('\\'); write(' ').
+	
+draw_line1sq2([]).
+draw_line1sq2([TEAM|[SIDES|NONE]]):-
+	member('n',SIDES) -> write('|'); write(' ').
+	
+draw_line1sq3([]).
+draw_line1sq3([TEAM|[SIDES|NONE]]):-
+	member('ne',SIDES) -> write('/'); write(' ').
+	
+draw_line2([]).
+draw_line2([SQUARE|REST]):-
+	draw_line2sq1(SQUARE),
+	draw_line2sq2(SQUARE),
+	draw_line2sq3(SQUARE),
+	write('|'),
+	draw_line2(REST).
+ 
+draw_line2sq1([]).
+draw_line2sq1([TEAM|[SIDES|NONE]]):-
+	member('w',SIDES) -> write('-'); write(' ').
+
+draw_line2sq2([]).
+draw_line2sq2([TEAM|[SIDES|NONE]]):-
+	=(TEAM,'green') -> write('X');
+	=(TEAM,'red') -> write('O') ; write(' ').
+	
+draw_line2sq3([]).
+draw_line2sq3([TEAM|[SIDES|NONE]]):-
+	member('e',SIDES) -> write('-'); write(' ').
+	
+draw_line3([]).
+draw_line3([SQUARE|REST]):-
+	draw_line3sq1(SQUARE),
+	draw_line3sq2(SQUARE),
+	draw_line3sq3(SQUARE),
+	write('|'),
+	draw_line3(REST).
+ 
+draw_line3sq1([]).
+draw_line3sq1([TEAM|[SIDES|NONE]]):-
+	member('sw',SIDES) -> write('/'); write(' ').
+	
+draw_line3sq2([]).
+draw_line3sq2([TEAM|[SIDES|NONE]]):-
+	member('s',SIDES) -> write('|'); write(' ').
+	
+draw_line3sq3([]).
+draw_line3sq3([TEAM|[SIDES|NONE]]):-
+	member('se',SIDES) -> write('\\'); write(' ').	
+ 
+draw_straightLine:-write('-------------------------------------').
