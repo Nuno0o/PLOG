46d6b64476ed78db2defc7d9e6b1b700cc74b7ce
nuno <nunojorge90@hotmail.com>
2016-12-05 18:44:28 +0000

    started print func

--- Trabalho2/src/crazypave.pl
+++ Trabalho2/src/crazypave.pl
@@ -1,10 +1,13 @@
 :- include('container.pl').
+:- include('solver.pl').
+:- include('printer.pl').
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
 
 
 test:-cpave1(A),lineRest1(X),colRest1(Y),setAreaByNumber(1,A,B),setAreaByNumber(7,B,C),setAreaByNumber(9,C,D),setAreaByNumber(3,D,E),setAreaByNumber(4,E,F),setAreaByNumber(8,F,G),setAreaByNumber(11,G,H),allConditionsMet(X,Y,H).
 test2:-cpave1(A),lineRest1(X),colRest1(Y),solveGame(A,X,Y,Sol),write(Sol).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %getCoord(+X,+Y,+Board,-N)
@@ -107,45 +110,4 @@ getShadedCol_aux(X,Y,Board,N1,N):-
     Y1 #= Y+1,
     getShadedCol_aux(X,Y1,Board,N1,N).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%allConditionsMet(+Board).
-allConditionsMet(Lines,Cols,Board):-
-    allLinesMet(Lines,Board),
-    allColsMet(Cols,Board).
-
-allLinesMet([],_).
-
-allLinesMet([Line,N|Rest],Board):-
-    getShadedLine(Line,Board,N),
-    allLinesMet(Rest,Board).
-
-allColsMet([],_).
-
-allColsMet([Col,N|Rest],Board):-
-
-    getShadedCol(Col,Board,N),
-    allColsMet(Rest,Board).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-shadeAllOnList(_,[],Board,Board).
-shadeAllOnList(Elem,[1|List],Board,NewBoard):-
-    setAreaByNumber(Elem,Board,Board1),
-    NextElem #= Elem+1,
-    shadeAllOnList(NextElem,List,Board1,NewBoard).
-
-shadeAllOnList(Elem,[0|List],Board,NewBoard):-
-    NextElem #= Elem+1,
-    shadeAllOnList(NextElem,List,Board,NewBoard).
-
-solveGame(Board,Lines,Cols,Elems):-
-    getBiggestN(Board,N),
-    length(Elems,N),
-    domain(Elems,0,1),
-    shadeAllOnList(1,Elems,Board,NewBoard),
-    allConditionsMet(Lines,Cols,NewBoard),
-    statistics(walltime,_),
-    labeling([],Elems),
-    fd_statistics.
     
---
+++ Trabalho2/src/printer.pl
@@ -0,0 +1,51 @@
+%normalPrint(+Board)
+normalPrint(Board):-
+    nth0(0,Board,Line),
+    length(Line,Size),
+    length(Selected,Size),
+    domain(Selected,0,0),
+    printB(Board,Selected).
+
+solvedPrint(Board,Solution):-
+    printB(Board,Solution).
+
+
+%print(+Board,-Shaded)
+printB(Board,Shaded):-
+    shadeAllOnList(1,Shaded,Board,ShadedBoard),
+    addWalls(ShadedBoard,WalledBoard).
+
+addWalls(ShadedBoard,WalledBoard):-
+    addWallsCols(ShadedBoard,WalledBoard1),
+    addWallsLines(WalledBoard1,WalledBoard).
+
+addWallsCols([],[]).
+addWallsCols([Line1|Rest1],[Line2|Rest2]):-
+    addWallsCols_aux(Line1,Line2),
+    addWallsCols(Rest1,Rest2).
+
+addWallsCols_aux([_|[]],[_|[]]).
+addWallsCols_aux([Elem1,Elem1|Rest1],[Elem1,1,Elem1|Rest2]):-
+    addWallsCols_aux([Elem1|Rest1],[Elem1|Rest2]).
+
+addWallsCols_aux([Elem1,Elem2|Rest1],[Elem1,0,Elem2|Rest2]):-
+    Elem2 \= Elem1,
+    addWallsCols_aux([Elem2|Rest1],[Elem2|Rest2]).
+
+addWallsLines([_|[]],[_|[]]).
+addWallsLines([Line1,Line2|Rest1],[Line1,WallLine,Line2|Rest2]):-
+    createWallLine(Line1,Line2,WallLine),
+    addWallsLines([Line2|Rest1],[Line2|Rest2]).
+
+createWallLine([],[],[]).
+createWallLine([Elem1|Rest1],[Elem1|Rest2],[1|Rest3]):-
+    skipElem(Rest1,Rest2,Rest3,NewRest1,NewRest2,NewRest3),
+    createWallLine(NewRest1,NewRest2,NewRest3).
+
+createWallLine([Elem1|Rest1],[Elem2|Rest2],[0|Rest3]):-
+    Elem1 \= Elem2,
+    skipElem(Rest1,Rest2,Rest3,NewRest1,NewRest2,NewRest3),
+    createWallLine(NewRest1,NewRest2,NewRest3).
+
+skipElem([],[],[],[],[],[]).
+skipElem([_|Rest1],[_|Rest2],[1|Rest3],Rest1,Rest2,Rest3).
