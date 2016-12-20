029e8d3163e51d11065d144c36afcbccf0c11fce
Goncalo Ribeiro <goncalo.ribeiro@fe.up.pt>
2016-12-20 04:44:10 +0000

    Fixed a comment

--- Trabalho2/src/crazypave.pl
+++ Trabalho2/src/crazypave.pl
@@ -4,7 +4,8 @@
 :- include('interface.pl').
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
-=======
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 test:-cpave1(A),lineRest1(X),colRest1(Y),setAreaByNumber(1,A,B),setAreaByNumber(7,B,C),setAreaByNumber(9,C,D),setAreaByNumber(3,D,E),setAreaByNumber(4,E,F),setAreaByNumber(8,F,G),setAreaByNumber(11,G,H),allConditionsMet(X,Y,H).
 test2:-cpave(1,A),lineRest1(X),colRest1(Y),solveGame(A,X,Y,Sol),printB(A,Sol).
