a6dfc92dd4aa6c99ae22c5e1885ac38187d054a6
nuno <nunojorge90@hotmail.com>
2016-12-20 13:22:50 +0000

    MUCH BETTER 0 BACKTRACKS

--- Trabalho2/src/crazypave.pl
+++ Trabalho2/src/crazypave.pl
@@ -15,34 +15,6 @@ getCoord(X,Y,Board,N):-
     nth0(Y,Board,Line),
     nth0(X,Line,N).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%setAreaByNumber(+N,+Board,-NewBoard)
-setAreaByNumber(N,Board,NewBoard):-
-    setArea_aux(Board,N,NewBoard).
-
-%setAreaAtCoord(+X,+Y,+Board,-NewBoard)
-setAreaAtCoord(X,Y,Board,NewBoard):-
-    getCoord(X,Y,Board,Item),
-    setArea_aux(Board,Item,NewBoard).
-
-
-setArea_aux([],_,[]).
-setArea_aux([Line|Tail1],Item,[NewLine|Tail2]):-
-    setArea_aux2(Line,Item,NewLine),
-    setArea_aux(Tail1,Item,Tail2).
-
-setArea_aux2([],_,[]).
-setArea_aux2([Item|Tail1],Item,[NewItem|Tail2]):-
-    NewItem #= -Item,
-    setArea_aux2(Tail1,Item,Tail2).
-
-setArea_aux2([Item1|Tail1],Item,[Item1|Tail2]):-
-    Item1 #\= Item,
-    setArea_aux2(Tail1,Item,Tail2).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 max(N,N1,N):-
     N1 =< N.
 
@@ -65,33 +37,46 @@ getBiggestN_aux2(Line,N):-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%getShadedLine(+Y,+Board,-N)
-getShadedLine(Y,Board,N):-
-    getShadedLine_aux(0,Y,Board,N).
+%getShadedLine(+Y,+Board,+N,+Elems)
+getShadedLine(Y,Board,N,Elems):-
+    getShadedLine_aux(0,Y,Board,N,Elems).
 
-getShadedLine_aux(Size,_,[Line|_],0):-
+getShadedLine_aux(Size,_,[Line|_],0,_):-
     length(Line,Size).
 
-getShadedLine_aux(X,Y,Board,N):-
+getShadedLine_aux(X,Y,Board,N,Elems):-
     getCoord(X,Y,Board,Elem),
-    Elem #< 0 #<=> B,
-    N1 #= N-B,
+    nth1(Elem,Elems,1),
+    N1 #= N-1,
     X1 #= X+1,
-    getShadedLine_aux(X1,Y,Board,N1).
+    getShadedLine_aux(X1,Y,Board,N1,Elems).
+
+getShadedLine_aux(X,Y,Board,N,Elems):-
+    getCoord(X,Y,Board,Elem),
+    nth1(Elem,Elems,0),
+    N1 #= N,
+    X1 #= X+1,
+    getShadedLine_aux(X1,Y,Board,N1,Elems).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %getShadedCol(+X,+Board,-N)
-getShadedCol(X,Board,N):-
-    getShadedCol_aux(X,0,Board,N).
+getShadedCol(X,Board,N,Elems):-
+    getShadedCol_aux(X,0,Board,N,Elems).
 
-getShadedCol_aux(_,Size,[Line|_],0):-
+getShadedCol_aux(_,Size,[Line|_],0,_):-
     length(Line,Size).
 
-getShadedCol_aux(X,Y,Board,N):-
+getShadedCol_aux(X,Y,Board,N,Elems):-
     getCoord(X,Y,Board,Elem),
-    Elem #< 0 #<=> B,
-    N1 #= N-B,
+    nth1(Elem,Elems,1),
+    N1 #= N-1,
     Y1 #= Y+1,
-    getShadedCol_aux(X,Y1,Board,N1).
+    getShadedCol_aux(X,Y1,Board,N1,Elems).
 
-    
+getShadedCol_aux(X,Y,Board,N,Elems):-
+    getCoord(X,Y,Board,Elem),
+    nth1(Elem,Elems,0),
+    N1 #= N,
+    Y1 #= Y+1,
+    getShadedCol_aux(X,Y1,Board,N1,Elems).
--- Trabalho2/src/solver.pl
+++ Trabalho2/src/solver.pl
@@ -1,39 +1,28 @@
 %allConditionsMet(+Board).
-allConditionsMet(Lines,Cols,Board):-
-    allLinesMet(Lines,Board),
-    allColsMet(Cols,Board).
+allConditionsMet(Lines,Cols,Board,Elems):-
+    allLinesMet(Lines,Board,Elems),
+    allColsMet(Cols,Board,Elems).
 
-allLinesMet([],_).
+allLinesMet([],_,_).
 
-allLinesMet([Line,N|Rest],Board):-
-    getShadedLine(Line,Board,N),
-    allLinesMet(Rest,Board).
+allLinesMet([Line,N|Rest],Board,Elems):-
+    getShadedLine(Line,Board,N,Elems),
+    allLinesMet(Rest,Board,Elems).
 
-allColsMet([],_).
+allColsMet([],_,_).
 
-allColsMet([Col,N|Rest],Board):-
+allColsMet([Col,N|Rest],Board,Elems):-
 
-    getShadedCol(Col,Board,N),
-    allColsMet(Rest,Board).
+    getShadedCol(Col,Board,N,Elems),
+    allColsMet(Rest,Board,Elems).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-shadeAllOnList(_,[],Board,Board).
-shadeAllOnList(Elem,[1|List],Board,NewBoard):-
-    setAreaByNumber(Elem,Board,Board1),
-    NextElem #= Elem+1,
-    shadeAllOnList(NextElem,List,Board1,NewBoard).
-
-shadeAllOnList(Elem,[0|List],Board,NewBoard):-
-    NextElem #= Elem+1,
-    shadeAllOnList(NextElem,List,Board,NewBoard).
 
 solveGame(Board,Lines,Cols,Elems):-
     getBiggestN(Board,N),
     length(Elems,N),
     domain(Elems,0,1),
-    shadeAllOnList(1,Elems,Board,NewBoard),
-    allConditionsMet(Lines,Cols,NewBoard),
+    allConditionsMet(Lines,Cols,Board,Elems),
     statistics(walltime,_),
     labeling([],Elems),
     fd_statistics.
