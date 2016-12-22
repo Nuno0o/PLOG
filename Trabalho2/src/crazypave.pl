f7bf06e27a8e9a97cb38efd70278e05107f826db
nuno <nunojorge90@hotmail.com>
2016-12-22 21:11:23 +0000

    last

--- Trabalho2/src/crazypave.pl
+++ Trabalho2/src/crazypave.pl
@@ -10,6 +10,7 @@
 test1:-cpave(1,A),lineRest1(X),colRest1(Y),solveGame(A,X,Y,Sol),printB(A,Sol).
 test2:-cpave(2,A),lineRest2(X),colRest2(Y),solveGame(A,X,Y,Sol),printB(A,Sol).
 test3:-cpave(3,A),lineRest3(X),colRest3(Y),solveGame(A,X,Y,Sol),printB(A,Sol).
+test4:-cpave(4,A),lineRest4(X),colRest4(Y),solveGame(A,X,Y,Sol),printB(A,Sol).
 crazypave :- 
 write('\n*---------------*\n| CRAZYPAVEMENT |\n*---------------*\n'),
 interface.
